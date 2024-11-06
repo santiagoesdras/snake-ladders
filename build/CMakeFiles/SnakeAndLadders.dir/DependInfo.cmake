f37cf9643c0fbcc3f8aff6bb0311b77a83f0719e
unknown <santiagoesdras501@gmail.com>
2024-11-06 03:39:49 -0600

    include buidl dir

---
+++ build/CMakeFiles/SnakeAndLadders.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/PC-JUANCHO/Desktop/Tareas/Segundo semestre/Programacion Avanzada/Practica/Proyecto2/src/Game.cpp" "CMakeFiles/SnakeAndLadders.dir/src/Game.cpp.obj" "gcc" "CMakeFiles/SnakeAndLadders.dir/src/Game.cpp.obj.d"
+  "C:/Users/PC-JUANCHO/Desktop/Tareas/Segundo semestre/Programacion Avanzada/Practica/Proyecto2/src/List.cpp" "CMakeFiles/SnakeAndLadders.dir/src/List.cpp.obj" "gcc" "CMakeFiles/SnakeAndLadders.dir/src/List.cpp.obj.d"
+  "C:/Users/PC-JUANCHO/Desktop/Tareas/Segundo semestre/Programacion Avanzada/Practica/Proyecto2/src/Queues.cpp" "CMakeFiles/SnakeAndLadders.dir/src/Queues.cpp.obj" "gcc" "CMakeFiles/SnakeAndLadders.dir/src/Queues.cpp.obj.d"
+  "C:/Users/PC-JUANCHO/Desktop/Tareas/Segundo semestre/Programacion Avanzada/Practica/Proyecto2/src/Render.cpp" "CMakeFiles/SnakeAndLadders.dir/src/Render.cpp.obj" "gcc" "CMakeFiles/SnakeAndLadders.dir/src/Render.cpp.obj.d"
+  "C:/Users/PC-JUANCHO/Desktop/Tareas/Segundo semestre/Programacion Avanzada/Practica/Proyecto2/src/Window.cpp" "CMakeFiles/SnakeAndLadders.dir/src/Window.cpp.obj" "gcc" "CMakeFiles/SnakeAndLadders.dir/src/Window.cpp.obj.d"
+  "C:/Users/PC-JUANCHO/Desktop/Tareas/Segundo semestre/Programacion Avanzada/Practica/Proyecto2/src/main.cpp" "CMakeFiles/SnakeAndLadders.dir/src/main.cpp.obj" "gcc" "CMakeFiles/SnakeAndLadders.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
